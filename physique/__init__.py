2fd66349c09ebfd6117e82bd8dc371c394a2664a
David <david.therincourt@orange.fr>
2020-03-17 08:46:39 +0400

    modifié :         physique/__init__.py 	modifié :         setup.py

--- physique/__init__.py
+++ physique/__init__.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 """
 Module Python 3 pour les sciences physiques au lycée.
@@ -6,33 +5,37 @@ Module Python 3 pour les sciences physiques au lycée.
 @author: David Thérincourt
 """
 
-
 import numpy as np
 from scipy.stats import linregress
 from scipy.optimize import curve_fit
 
 def ajustement_affine(x, y):
     """
-    Modélistion d'un fonction affine.
-    x et y sont des listes ou tableaux Numpy de même taille
-    Retourne le coef. directeur a et l'ordonnée à l'origine b
+    Modélisation d'une fonction affine y = a*x + b
+    x : liste ou tableau Numpy.
+    y : liste ou tableau Numpy de même dimension que x.
     """
     a, b, _, _, _ = linregress(x,y)
     return a, b
 
 def exp1(x, A, tau, x0=0):        
     """
-    Fonction exponenetielle croissante du type
-    y = A*(1-exp(-(x-xo)/tau))
+    Fonction exponenetielle croissante y = A*(1-exp(-(x-x0)/tau))
+    x   : liste ou tableau Numpy.
+    A   : valeur à l'infini de y
+    tau : constante de temps
+    x0  : retard
     """          
     return A*(1-np.exp(-(x-x0)/tau))    
 
-def ajustement_exp1(x, y, A0=1, tau0=1, X0=1) :
+def ajustement_exp1(x, y, A0=1, tau0=1, X0=0) :
     """
-    Modélisation d'une série de points (x,y) 
-    par une fonction exponentielle croissante du type y = A*(1-exp(-(x-xo)/tau))
-    Retourne les paramètres A, tau et xo
+    Modélisation d'une série de points (x,y) par une fonction
+    exponentielle croissante du type y = A*(1-exp(-(x-xo)/tau))
+    x : liste ou tableau Numpy.
+    y : liste ou tableau Numpy de même dimension que x.
+    A0, tau0 et X0 : optionnels
+    Retourne (A, tau, x0)
     """
     (A,tau,x0), pcov = curve_fit(exp1,x,y, p0=[A0, tau0, X0])
-    
     return A, tau, x0
--- setup.py
+++ setup.py
@@ -1,22 +1,11 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="physiquepy", # Replace with your own username
-    version="0.0.1",
-    author="THERINCOURT David",
-    author_email="",
-    description="Une librairie Python pour les sciences physiques",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/david-therincourt/physique",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setup(name='physique',
+      version='0.1',
+      description='Librairie pour les sciences physiques',
+      url='http://github.com/david-therincourt/physique',
+      author='David THERINCOURT',
+      author_email='dtherincourt@gmail.com',
+      license='MIT',
+      packages=['physique'],
+      zip_safe=False)
