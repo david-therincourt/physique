52144391a455094d44658c8fac35fe5e0d38550b
david-therincourt <david.therincourt@orange.fr>
2022-09-26 22:17:50 +0400

    nouveau fichier : tests/csv/exemple_2.png 	nouveau fichier : tests/csv/exemple_2.py 	nouveau fichier : tests/signal/exemple_3.png 	nouveau fichier : tests/signal/exemple_3.py

---
+++ tests/csv/exemple_2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from physique import load_avimeca3_txt
+
+t, x, y = load_avimeca3_txt('data1_avimeca3.txt')
+
+plt.plot(x,y,'.')
+plt.title("Trajectoire d'un ballon")
+plt.xlabel('x (m)')
+plt.ylabel('y (m)')
+plt.grid()
+plt.savefig("exemple_2.png")
+plt.show()
---
+++ tests/signal/exemple_3.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from physique import load_oscillo_csv, integre
+
+t, u = load_oscillo_csv('scope_integre.csv')
+
+f = 125
+T = 1/f
+aire = integre(t, u, 0, T, plot_axes=plt)
+moy = aire/T
+
+
+plt.plot(t, u)
+plt.axhline(moy, ls="--", color="C3")
+plt.text(0.65*T, moy+0.2, "Moy = {:.2f} V".format(moy), color="C3")
+plt.title("Valeur moyenne d'un signal périodique")
+plt.xlabel("t (s)")
+plt.ylabel("u (V)")
+plt.grid()
+plt.savefig("exemple_3.png")
+plt.show()
